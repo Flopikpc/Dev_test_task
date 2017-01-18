c71b4fc6fe72026b500f43edd262c85b50e9dd15
Flopikpc <flopikpc@gmail.com>
2017-01-18 16:52:47 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev3.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev3.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dev3/public_html/catalog/');
+define('DIR_SYSTEM', '/home/dev3/public_html/system/');
+define('DIR_LANGUAGE', '/home/dev3/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/dev3/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/dev3/public_html/system/config/');
+define('DIR_IMAGE', '/home/dev3/public_html/image/');
+define('DIR_CACHE', '/home/dev3/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/dev3/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/dev3/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/dev3/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/dev3/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'user1234');
+define('DB_DATABASE', 'developer_3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
